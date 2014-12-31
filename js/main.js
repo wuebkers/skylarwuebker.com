7940433ce0f5ddacfbbcaf29591b1f71cee68a0f
akrigline <andrew@akrigline.com>
2014-12-31 14:07:41 -0500

    Isotope Works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+var $container = $('.items').isotope({
+	itemselector: 'li',
+});
 
+$('#filters').on( 'click', 'a.filter', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+});
