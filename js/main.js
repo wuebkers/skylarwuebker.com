22940833a533753f0ae6ac66b9eb6d10a26bc06a
akrigline <andrew@akrigline.com>
2014-12-31 16:17:51 -0500

    Mobile Nav Finally Works
    Finally

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+$('#menu-btn').sidr({
+  name: 'sidr-main',
+  source: '#mobile-nav'
+});
+
 var $container = $('.items').isotope({
 	itemselector: 'li',
 });
 
-$('#filters').on( 'click', 'a.filter', function() {
+$('.filters').on( 'click', 'a.filter', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+});
+
+$('.sidr').on( 'click', 'a.sidr-class-filter', function() {
   var filterValue = $(this).attr('data-filter');
   $container.isotope({ filter: filterValue });
 });
